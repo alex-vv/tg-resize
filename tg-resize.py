ad52039925fa7c9005188654f9b40a00f6f3762a
Alexander Vykhodtsev <a.a.vykhodtsev@gmail.com>
2025-02-15 23:00:33 +0600

    Add an option for target dir, process all files in subfolders

--- tg-resize.py
+++ tg-resize.py
@@ -1,19 +1,25 @@
-# resize all jpg files in the current dir for telegram (1280px wide)
+# resize & sharpen all jpg files in the target dir for Telegram (1280px wide)
 # resized files are prefixed with 'resized-' and excluded from subsequent resize
+# original files are deleted
 
 import pyvips
 import os
+import sys
 
 format="jpg"
 prefix="resized-"
 
-def resize(file):
+dir = sys.argv[1] if len(sys.argv) > 1 else '.'
+
+def resize(path, filename):
+	file = os.path.join(path, filename)
+	print("resizing " + file)
 	image = pyvips.Image.thumbnail(file, 1280)
-	image.sharpen(sigma=0.5, x1=2, y2=10, y3=20, m1=1.0, m2=3)
-	image.write_to_file(prefix + file, keep="none")
+	image = image.sharpen(sigma=0.5, x1=2, y2=5, y3=20, m1=0.5, m2=1)
+	image.write_to_file(os.path.join(path, prefix + filename), keep="none")
 	os.remove(file)
 
-files = [f for f in os.listdir('.') if os.path.isfile(f) and f.endswith("." + format) and not f.startswith(prefix)]
-for f in files:
-	resize(f)
-
+for dirpath, dirnames, filenames in os.walk(dir):
+   for filename in filenames:
+      if filename.endswith("." + format) and not filename.startswith(prefix):
+         resize(dirpath, filename)
