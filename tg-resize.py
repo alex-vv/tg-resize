8bddfc1e3dfec554c09f13700a65cfccf8f31a61
Alexander Vykhodtsev <a.a.vykhodtsev@gmail.com>
2025-04-06 22:03:24 +0600

    Increased size to 2560px (with Send large photos experimental setting)

--- tg-resize.py
+++ tg-resize.py
@@ -1,4 +1,4 @@
-# resize & sharpen all jpg files in the target dir for Telegram (1280px wide)
+# resize & sharpen all jpg files in the target dir for Telegram (2560px wide)
 # resized files are prefixed with 'resized-' and excluded from subsequent resize
 # original files are deleted
 
@@ -8,13 +8,14 @@ import sys
 
 format="jpg"
 prefix="resized-"
+max_size=2560
 
 dir = sys.argv[1] if len(sys.argv) > 1 else '.'
 
 def resize(path, filename):
 	file = os.path.join(path, filename)
 	print("resizing " + file)
-	image = pyvips.Image.thumbnail(file, 1280)
+	image = pyvips.Image.thumbnail(file, max_size)
 	image = image.sharpen(sigma=0.5, x1=2, y2=5, y3=20, m1=0.5, m2=1)
 	image.write_to_file(os.path.join(path, prefix + filename), keep="none")
 	os.remove(file)
